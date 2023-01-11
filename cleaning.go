066168c7b8b089aa57d4474719dcdcdbd13a12c3
Pascal Castéran <15686972+pcasteran@users.noreply.github.com>
2023-01-11 15:26:51 +0100

    Up

--- cleaning.go
+++ cleaning.go
@@ -22,10 +22,10 @@ func clean(ghClient *GithubClient, regClient *ContainerRegistryClient, user, pkg
 	}
 
 	// Get the registry object (image or image index) for each hash.
-	log.Debug().Str("user", user).Str("package", pkg).Msg("fetching the container registry objects")
 	repository := fmt.Sprintf("%s/%s/%s", registry, user, pkg)
+	log.Debug().Str("repository", repository).Msg("fetching the container registry objects")
 	imageByHash := make(map[string]v1.Image)
-	imageIndexByHash := make(map[string]v1.ImageIndex)
+	indexByHash := make(map[string]v1.ImageIndex)
 	for hash := range packageVersionByHash {
 		log.Trace().Str("hash", hash).Msg("fetching container registry object")
 
@@ -39,7 +39,7 @@ func clean(ghClient *GithubClient, regClient *ContainerRegistryClient, user, pkg
 		if image != nil {
 			imageByHash[hash] = image
 		} else if index != nil {
-			imageIndexByHash[hash] = index
+			indexByHash[hash] = index
 		} else {
 			// Something went wrong, we should never be here...
 			log.Warn().Err(err).Msg("invalid container registry object, that should not happen")
@@ -48,14 +48,42 @@ func clean(ghClient *GithubClient, regClient *ContainerRegistryClient, user, pkg
 	}
 
 	// Determine the package versions to delete.
+	toDelete, err := computeHashesToDelete(ghClient, imageByHash, indexByHash, prTagRegex)
+	if err != nil {
+		return fmt.Errorf("unable to compute the hashed to delete: %w", err)
+	}
 
 	// Delete them.
 	if !dryRun {
-		// TODO
+		// No dry run, perform the deletion.
+		nbDeleted := 0
+		for _, hash := range toDelete {
+			version := packageVersionByHash[hash]
+			log.Trace().Str("hash", hash).Int64("version-id", *version.ID).Msg("Deleting package version")
+			nbDeleted++ // TODO: increment if no error during deletion
+		}
+
+		log.Info().Int("nb-deleted", nbDeleted).Msg("Registry cleaning done")
 	} else {
 		// Dry run mode, don't perform the deletion.
-		log.Info().Msg("Dry run mode is ON, no deletion has been performed.")
+		log.Info().Msg("Dry run mode is ON, no deletion has been performed")
 	}
 
 	return nil
+}
+
+func computeHashesToDelete(
+	ghClient *GithubClient,
+	imageByHash map[string]v1.Image,
+	indexByHash map[string]v1.ImageIndex,
+	prTagRegex *regexp.Regexp) ([]string, error) {
+	// Create a string set containing the hashes to delete.
+	toDelete := make(map[string]struct{})
+
+	// Return a slice of the hashes to delete.
+	var ret []string
+	for hash := range toDelete {
+		ret = append(ret, hash)
+	}
+	return ret, nil
 }
